26e4dc9abbd35f7819d4dd1578dc86b388ec64b4
gonzalovazquez <gonzalo.segura1@gmail.com>
2016-06-04 19:24:42 -0400

    removed dist

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.set('port', (process.env.PORT || 5000));
 var jenkins = jenkinsapi.init("https://ci.jenkins.io/");
 
 // Initialized 
-app.use('/', express.static('dist'));
+app.get('/', function(req, res) {
+  res.status(200).send('API available');
+});
 
 
 // GET all jobs
@@ -35,5 +37,5 @@ app.get('/api/job/:id', function (req, res) {
 
 // Run server
 app.listen(app.get('port'), function () {
-  console.log('Build Health Dashboard API listening on port: ' + app.get('port'));
+  console.log('Build Health Dashboard API listening on port: http://localhost:' + app.get('port'));
 });
